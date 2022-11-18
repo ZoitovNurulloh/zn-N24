57859ba3ba39658f7ccd9b5ccb98e0e18b405743
ZoitovNurillo <nurillozoitov97@gmail.com>
2022-11-18 20:44:54 +0500

    Add icon close and modal

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const elModal = document.querySelector(".modal");
+const elModalClose = elModal.querySelector(".js-modal-close");
+
+if (elModalClose) {
+  elModalClose .addEventListener("click" ,  function () {
+    elModal.classList.remove("modal-open")
+  })
+}
